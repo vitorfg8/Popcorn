aa8fe2ed8e399f9d8343009ca3c2265d1099d342
Vítor Ferreira Guimarães <vitorfer237@gmail.com>
2023-08-31 20:48:57 -0300

    refactor home activity

--- app/src/main/java/com/github/vitorfg8/popcorn/home/HomeActivity.kt
+++ app/src/main/java/com/github/vitorfg8/popcorn/home/HomeActivity.kt
@@ -3,16 +3,10 @@ package com.github.vitorfg8.popcorn.home
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.github.vitorfg8.popcorn.R
-import com.github.vitorfg8.popcorn.home.trends.ui.TrendsFragment
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-        val trendsFragment = TrendsFragment.newInstance()
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainer, trendsFragment)
-            .commit()
     }
 }
