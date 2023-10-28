49fd22879bfa9e19d9c0130555dd340b06496250
Vítor Ferreira <vitorfer237@gmail.com>
2023-10-28 17:19:17 -0300

    fix PopularMoviesService

--- app/src/main/java/com/github/vitorfg8/popcorn/home/popularmovies/data/service/PopularMoviesService.kt
+++ app/src/main/java/com/github/vitorfg8/popcorn/home/popularmovies/data/service/PopularMoviesService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Query
 import java.util.Locale
 
 interface PopularMoviesService {
-    @GET("movie/popular/")
+    @GET("movie/popular")
     suspend fun getPopularMovies(
         @Query("api_key") apiKey: String = BuildConfig.API_KEY,
         @Query("language") language: String = Locale.getDefault().toLanguageTag(),
