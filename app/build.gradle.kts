41ddc328626e6e46cbf3fa8c6b2076513e86a860
Vítor Ferreira Guimarães <vitorfer237@gmail.com>
2024-09-10 14:35:32 -0300

    update the version of the libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.github.vitorfg8.popcorn"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.github.vitorfg8.popcorn"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
